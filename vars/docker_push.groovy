a3a38fa85323806f68f4f7ad734a7102716fffae
Parag Soni <75782572+Paragsoni-max@users.noreply.github.com>
2024-11-17 19:17:11 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,22 +1,22 @@
-def call(String DockerHubUser, String ProjectName, String ImageTag, String BuildNumber){
-       if (!DockerHubUser || !ProjectName || !ImageTag || !BuildNumber) {
+def call(String DockerHubUser, String ProjectName, String ImageTag = "latest", String BuildNumber){
+    if (!DockerHubUser || !ProjectName || !ImageTag || !BuildNumber) {
         error "Missing required parameters. Ensure DockerHubUser, ProjectName, ImageTag, and BuildNumber are set."
     }
-    
+
     echo "Tagging and pushing Docker image:"
     echo "DockerHubUser: ${DockerHubUser}, ProjectName: ${ProjectName}, ImageTag: ${ImageTag}, BuildNumber: ${BuildNumber}"
 
-   withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials',
-                    usernameVariable: 'DOCKER_USERNAME',
-                    passwordVariable: 'DOCKER_PASSWORD')]) {
-                        sh '''
-                            docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-                            docker tag ${DockerHubUser}/${ProjectName}:${ImageTag} ${DockerHubUser}/${ProjectName}:${BuildNumber}
-                            docker push ${DockerHubUser}/${ProjectName}:${BuildNumber}
-
-                            # Also push the latest tag
-                            docker tag ${DockerHubUser}/${ProjectName}:latest ${DockerHubUser}/${ProjectName}:latest
-                            docker push ${DockerHubUser}/${ProjectName}:latest
-                        '''
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials',
+                     usernameVariable: 'DOCKER_USERNAME',
+                     passwordVariable: 'DOCKER_PASSWORD')]) {
+        sh """
+            set -e
+            docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+            docker tag ${DockerHubUser}/${ProjectName}:${ImageTag} ${DockerHubUser}/${ProjectName}:${BuildNumber}
+            docker push ${DockerHubUser}/${ProjectName}:${BuildNumber}
+            
+            docker tag ${DockerHubUser}/${ProjectName}:${ImageTag} ${DockerHubUser}/${ProjectName}:latest
+            docker push ${DockerHubUser}/${ProjectName}:latest
+        """
     }
 }
