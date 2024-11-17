382ad09c84a49d69807ec933ba10dc66bf686bd6
Parag Soni <75782572+Paragsoni-max@users.noreply.github.com>
2024-11-17 16:34:17 +0530

    Create install_dependency.groovy

---
+++ vars/install_dependency.groovy
@@ -0,0 +1,16 @@
+def call(Number NodeVersion) {
+    sh '''
+        export NVM_DIR="$HOME/.nvm"
+        [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+        
+        if ! nvm ls "${NodeVersion}" | grep -q "${NodeVersion}"; then
+            echo "Node.js version ${NodeVersion} not found. Installing..."
+            nvm install "${NodeVersion}"
+        else
+            echo "Node.js version ${NodeVersion} is already installed."
+        fi
+
+        nvm use "${NodeVersion}"
+        npm install
+    '''
+}
