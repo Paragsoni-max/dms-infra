8aebd6d9d795f73a5aa4883f45d77ed1be41b268
Parag Soni <75782572+Paragsoni-max@users.noreply.github.com>
2024-11-17 19:33:01 +0530

    Update deployment.groovy

--- vars/deployment.groovy
+++ vars/deployment.groovy
@@ -3,18 +3,19 @@ def call(String FrontendVMIP, String SshCredentialsId, String DockerHubUser, Str
         sh """
         ssh -i \$SSH_KEY_FILE -o StrictHostKeyChecking=no \$SSH_USER@$FrontendVMIP << EOF
         # Pull the latest Docker image from Docker Hub
-        docker pull ${DockerHubUser}/${ProjectName}:${ImageTag}
+        docker pull ${DockerHubUser}\\/${ProjectName}:${ImageTag}
         
         echo 'Stopping and removing all containers if they exist...'
         # Stop and remove all containers
-        if [ $(docker ps -aq) ]; then
-        docker stop $(docker ps -aq) || true
-        docker rm $(docker ps -aq) || true
+        if [ \$(docker ps -aq) ]; then
+            docker stop \$(docker ps -aq) || true
+            docker rm \$(docker ps -aq) || true
         fi
         
         # Run the new container in detached mode on port 80
-        docker run -d -p 80:80 --name ${ContainerName} ${DockerHubUser}/${ProjectName}:${ImageTag}
-       << EOF
+        docker run -d -p 80:80 --name ${ContainerName} ${DockerHubUser}\\/${ProjectName}:${ImageTag}
+        << EOF
         """
     }
 }
+
