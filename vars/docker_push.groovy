b232f9a4290ac996c2d7e9c69d43d0f75df3c212
Parag Soni <75782572+Paragsoni-max@users.noreply.github.com>
2024-11-17 19:14:03 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,4 +1,11 @@
 def call(String DockerHubUser, String ProjectName, String ImageTag, String BuildNumber){
+       if (!DockerHubUser || !ProjectName || !ImageTag || !BuildNumber) {
+        error "Missing required parameters. Ensure DockerHubUser, ProjectName, ImageTag, and BuildNumber are set."
+    }
+    
+    echo "Tagging and pushing Docker image:"
+    echo "DockerHubUser: ${DockerHubUser}, ProjectName: ${ProjectName}, ImageTag: ${ImageTag}, BuildNumber: ${BuildNumber}"
+
    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials',
                     usernameVariable: 'DOCKER_USERNAME',
                     passwordVariable: 'DOCKER_PASSWORD')]) {
