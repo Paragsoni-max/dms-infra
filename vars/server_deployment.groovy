4caa49f8f08e263e97f0337c62218305bc11f250
Parag Soni <75782572+Paragsoni-max@users.noreply.github.com>
2024-11-25 12:10:19 +0530

    Update server_deployment.groovy

--- vars/server_deployment.groovy
+++ vars/server_deployment.groovy
@@ -7,5 +7,28 @@ def call(String BackendVMIP, String SshCredentialsId, String DockerHubUser, Stri
     echo "ImageTag: ${ImageTag}"
     echo "ContainerName: ${ContainerName}"
     
+     withCredentials([sshUserPrivateKey(credentialsId: SshCredentialsId, keyFileVariable: 'SSH_KEY_FILE', usernameVariable: 'SSH_USER')]) {
+        // Log SSH details
+        echo "SSH_USER: \$SSH_USER"
+        echo "SSH_KEY_FILE: \$SSH_KEY_FILE"
+        
+        sh """
+        ssh -i \$SSH_KEY_FILE -o StrictHostKeyChecking=no \$SSH_USER@$BackendVMIP << EOF
+        # Log connection status
+        echo "Connected to ${BackendVMIP}"
+        
+        // # Stop and remove existing container
+        // docker stop ${ContainerName} || true
+        // docker rm ${ContainerName} || true
+        
+        // # Pull the latest Docker image from Docker Hub
+        // echo "Pulling image: ${DockerHubUser}/${ProjectName}:${ImageTag}"
+        // docker pull ${DockerHubUser}/${ProjectName}:${ImageTag}
+        
+        // # Run the new container in detached mode on port 5000
+        // echo "Starting container: ${ContainerName}"
+        // docker run --restart always --name ${ContainerName} -p 5000:5000 ${DockerHubUser}/${ProjectName}:${ImageTag}
+        // EOF
+        """
 
 }
